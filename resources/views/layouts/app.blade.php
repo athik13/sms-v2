2ed21f060060d03c484e31fe280025cff52e553f
Mohamed Athik <athik.13@gmail.com>
2020-08-13 18:30:48 +0800

    gs line

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,22 @@
         .iti.iti--allow-dropdown {
             width: 100%;
         }
+        .divide {
+            padding: 0;
+            margin: 0;
+            margin-bottom: 30px;
+            background: #1e5799;
+            background: -moz-linear-gradient(left,  #1e5799 0%, #f300ff 50%, #e0ff00 100%);
+            background: -webkit-gradient(linear, left top, right top, color-stop(0%,#1e5799), color-stop(50%,#f300ff), color-stop(100%,#e0ff00));
+            background: -webkit-linear-gradient(left,  #1e5799 0%,#f300ff 50%,#e0ff00 100%);
+            background: -o-linear-gradient(left,  #1e5799 0%,#f300ff 50%,#e0ff00 100%);
+            background: -ms-linear-gradient(left,  #1e5799 0%,#f300ff 50%,#e0ff00 100%);
+            background: linear-gradient(to right,  #1e5799 0%,#f300ff 50%,#e0ff00 100%);
+            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#e0ff00',GradientType=1 );
+
+            height: 3px;
+            border-bottom: 1px solid #000;
+        }
     </style>
 </head>
 <body class="bootstrap-dark">
@@ -97,6 +113,7 @@
                 </div>
             </div>
         </nav>
+        <div class="divide"></div>
 
         <main class="py-4">
             @yield('content')
